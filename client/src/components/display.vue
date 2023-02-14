003263055603c2ed98ab3f6afa9cece777b7d7e4
BrenanMarenger <19bkmare@gmail.com>
2023-02-13 19:40:48 -0500

    feat(display): skips, playback speed, captions

--- client/src/components/display.vue
+++ client/src/components/display.vue
@@ -5,6 +5,7 @@
                 <div class="title">
 
                 </div>
+                <!-- PLAY/PAUSE -->
                 <div class="controls">
                     <button class="playpause-btn" >
                         <svg @click="togglePlay" class="play-icon"  viewBox="0 0 24 24" >
@@ -14,6 +15,7 @@
                             <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
                         </svg>
                     </button>
+                    <!-- VOLUME -->
                     <div class="volume-container">
                         <button class="volume-btn">
                             <svg @click="toggleMute" v-if="volumeLevel > 0.5 && isMuted == false" class="volume-high-icon" viewBox="0 0 24 24">
@@ -28,6 +30,7 @@
                             <input type="range" @change="changeVolume($event)" class="volume-slider" min="0" max="1" step="any" :value="volumeLevel"> 
                         </button>
                     </div>
+                    <!-- TIME -->
                     <div class="video-duration-container">
                         <div class="current-time">
                             {{ current }}
@@ -37,12 +40,26 @@
                             {{ total }}
                         </div>
                     </div>
+                    <!-- SKIP -->
                     <button class="rewind">
-
+                        <svg @click="rewind" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M11.99,5V1l-5,5l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6h-2c0,4.42,3.58,8,8,8s8-3.58,8-8S16.41,5,11.99,5z"/><g>
+                            <path d="M10.89,16h-0.85v-3.26l-1.01,0.31v-0.69l1.77-0.63h0.09V16z"/><path d="M15.17,14.24c0,0.32-0.03,0.6-0.1,0.82s-0.17,0.42-0.29,0.57s-0.28,0.26-0.45,0.33s-0.37,0.1-0.59,0.1 s-0.41-0.03-0.59-0.1s-0.33-0.18-0.46-0.33s-0.23-0.34-0.3-0.57s-0.11-0.5-0.11-0.82V13.5c0-0.32,0.03-0.6,0.1-0.82 s0.17-0.42,0.29-0.57s0.28-0.26,0.45-0.33s0.37-0.1,0.59-0.1s0.41,0.03,0.59,0.1c0.18,0.07,0.33,0.18,0.46,0.33 s0.23,0.34,0.3,0.57s0.11,0.5,0.11,0.82V14.24z M14.32,13.38c0-0.19-0.01-0.35-0.04-0.48s-0.07-0.23-0.12-0.31 s-0.11-0.14-0.19-0.17s-0.16-0.05-0.25-0.05s-0.18,0.02-0.25,0.05s-0.14,0.09-0.19,0.17s-0.09,0.18-0.12,0.31 s-0.04,0.29-0.04,0.48v0.97c0,0.19,0.01,0.35,0.04,0.48s0.07,0.24,0.12,0.32s0.11,0.14,0.19,0.17s0.16,0.05,0.25,0.05 s0.18-0.02,0.25-0.05s0.14-0.09,0.19-0.17s0.09-0.19,0.11-0.32s0.04-0.29,0.04-0.48V13.38z"/></g></g></g></svg>
                     </button>
                     <button class="fast-forward">
-                        
+                        <svg @click="forward" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g>
+                            <path d="M18,13c0,3.31-2.69,6-6,6s-6-2.69-6-6s2.69-6,6-6v4l5-5l-5-5v4c-4.42,0-8,3.58-8,8c0,4.42,3.58,8,8,8s8-3.58,8-8H18z"/><polygon points="10.86,15.94 10.86,11.67 10.77,11.67 9,12.3 9,12.99 10.01,12.68 10.01,15.94"/><path d="M12.25,13.44v0.74c0,1.9,1.31,1.82,1.44,1.82c0.14,0,1.44,0.09,1.44-1.82v-0.74c0-1.9-1.31-1.82-1.44-1.82 C13.55,11.62,12.25,11.53,12.25,13.44z M14.29,13.32v0.97c0,0.77-0.21,1.03-0.59,1.03c-0.38,0-0.6-0.26-0.6-1.03v-0.97 c0-0.75,0.22-1.01,0.59-1.01C14.07,12.3,14.29,12.57,14.29,13.32z"/></g></g></svg>
                     </button>
+                    <!-- SPEED -->
+                    <button @click="changeSpeed" class="speed-btn text-wide">
+                        x{{speed}}
+                    </button>
+                    <!-- CAPTIONS -->
+                    <button >
+                        <svg class="subtitle-btn" @click="toggleCaptions" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M18,11H16.5V10.5H14.5V13.5H16.5V13H18V14A1,1 0 0,1 17,15H14A1,1 0 0,1 13,14V10A1,1 0 0,1 14,9H17A1,1 0 0,1 18,10M11,11H9.5V10.5H7.5V13.5H9.5V13H11V14A1,1 0 0,1 10,15H7A1,1 0 0,1 6,14V10A1,1 0 0,1 7,9H10A1,1 0 0,1 11,10M19,4H5C3.89,4 3,4.89 3,6V18A2,2 0 0,0 5,20H19A2,2 0 0,0 21,18V6C21,4.89 20.1,4 19,4Z" />
+                        </svg>
+                       </button>
+                    <!-- FULL SCREEN -->
                     <button class="full-screen-btn">
                         <svg @click="toggleFullScreen" class="open" viewBox="0 0 24 24">
                             <path   fill="currentColor" d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
@@ -56,7 +73,7 @@
             <video @click="togglePlay" v-if="displayVideo" :src="displayVideo.Path" autoplay>
                 <track
                 label="English"
-                kind="subtitles"
+                kind="captions"
                 srclang="en"
                 src=""
                 default
@@ -80,7 +97,8 @@ export default {
             volumeLevel: .25,
             current: 0,
             total: 0,
-            isMuted: false
+            isMuted: false,
+            speed: 1
             
         }
     },
@@ -104,9 +122,7 @@ export default {
         xmlhttp.send();
 
         //Getting video duration
-        setInterval(this.getCurrent, 500)
-        //this.getTotalTime()
-
+        setInterval(this.getCurrent, 250)
         
     },
     created() {
@@ -127,6 +143,16 @@ export default {
                 case "m":
                     this.toggleMute
                     break
+                case "c":
+                    this.toggleCaptions()
+                    break
+                case "arrowright":
+                case "l":
+                    this.forward()
+                    break
+                case "arrowleft":
+                case "j":
+                    this.rewind()
             }
         })
         
@@ -166,6 +192,19 @@ export default {
             }
         },
 
+        toggleCaptions(){
+            const video = document.querySelector("video")
+            const captions = video.textTracks[0]
+            const captionsBtn = document.querySelector(".subtitle-btn")
+            if(captions.mode === "hidden"){
+                captions.mode = "showing"
+                captionsBtn.classList.add(".underline")
+            } else {
+                captions.mode = "hidden"
+            }
+
+        },
+
         changeVolume(event){
             const video = document.querySelector("video")
 
@@ -200,6 +239,24 @@ export default {
         // this.total = this.formatTime(video.duration)
         // console.log("total: " + this.total)
         // }
+        forward(){
+            const video = document.querySelector("video")
+            video.currentTime += 10
+
+        },
+        rewind(){
+            const video = document.querySelector("video")
+            video.currentTime -= 10
+        },
+        changeSpeed(){
+            const video = document.querySelector("video")
+            this.speed = video.playbackRate + .25
+            if(this.speed > 2){
+                this.speed = 1
+            }
+            video.playbackRate = this.speed
+            
+        }
     },
     computed: {
     },
@@ -251,6 +308,10 @@ svg:hover{
     transition: 150ms ease-in-out;
 }
 
+.underline{
+    border-bottom: 3px solid black;
+}
+
 .video-container.full-screen{
     max-height: 100vh;
     max-width: initial;
@@ -279,6 +340,10 @@ svg:hover{
     cursor: pointer;
     opacity: .85;
     transition: opacity 150ms ease-in-out;
+}
+
+.text-wide{
+    width: 50px
 }
 
 .controls button:hover{
