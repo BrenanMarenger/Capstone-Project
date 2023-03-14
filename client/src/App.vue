31db691ca7e6aa653bffaac13c79b2472fc02747
BrenanMarenger <19bkmare@gmail.com>
2023-03-14 12:16:38 -0400

    feat(Loading): added transition for better flow

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     
-    <loader v-if="isLoading" />
+    <transition name="fade">
+      <loader v-if="isLoading" />
+    </transition>
     
     <router-view/>
     
@@ -27,6 +29,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s;
+}
+/* .fade-enter, */
+ .fade-leave-to {
+  opacity: 0;
+  scale: 1.2;
+}
 
 </style>
