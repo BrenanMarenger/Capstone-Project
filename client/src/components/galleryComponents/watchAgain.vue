7b2cc154d5cbb1f78bcebd12f7a07d01c34ed60c
BrenanMarenger <bmarenge@nmu.edu>
2023-03-18 09:36:38 -0400

    added carousel

--- client/src/components/galleryComponents/watchAgain.vue
+++ client/src/components/galleryComponents/watchAgain.vue
@@ -3,7 +3,7 @@
     <h1 class="title"> Watch Again </h1>
 
     <div class="watch-again-container"> 
-        <div class="side-buttons">
+        <div class="side-buttons" v-if="watchAgainVideos.length > 7">
                 <svg 
                 class="left-arrow" 
                 @click="scrollLeft"
@@ -21,28 +21,41 @@
 
                     <div class="watch-again-controls">
                         <div class="watch-again-title">
-                            <h1>{{ video.Title }}</h1>
+                            {{ video.Title }}
                         </div>
-                        <router-link class="play" :to="{name: 'display', params: {videoId: video.id}, query: { time: video.Spot }}" tag="button">
+                        <router-link 
+                            class="play" 
+                            :to="{name: 'display', params: {videoId: video.id}, query: { time: video.Spot }}" 
+                            tag="button">
                         <svg viewBox="0 0 24 24" >
                             <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
                         </svg>
                         </router-link>
-                        <button class="more-info" @click="sendToggleModal(video)">
+                        <button 
+                            class="more-info" 
+                            @click="sendToggleModal(video)">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
                         </button>
-                        <button @click="sendToggleFavorite(video)" v-if="favoritesId.includes(video.id)">
+                        <button 
+                            class="fav" 
+                            @click="sendToggleFavorite(video)" 
+                            v-if="favoritesId.includes(video.id)">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         </button>
-                        <button @click="sendToggleFavorite(video)" v-if="!(favoritesId.includes(video.id))">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                        <button 
+                            class="fav" 
+                            @click="sendToggleFavorite(video)" 
+                            v-if="!(favoritesId.includes(video.id))">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
                         </button> 
+                        <div class="item-categories">
+                            {{ video.Categories }}
+                        </div>
                     </div>
-                
             </div>
         </div>
 
-        <div class="side-buttons">
+        <div class="side-buttons" v-if="watchAgainVideos.length > 7">
                 <svg  
                 @click="scrollRight"
                 class="right-arrow" 
@@ -133,10 +146,10 @@ img{
 }
 .watch-again-title{
     color: white;
-  font-weight: 700;
-  font-size: 15px;
-  padding: 5px;
-  background: rgb(18, 18, 18);
+    font-weight: 700;
+    font-size: 20 !important;
+    padding: 9px;
+    margin: 1px;
 }
 
 button{
@@ -169,13 +182,16 @@ button{
 
 .watch-again-controls{
     position:relative;
-  display: none;
-  background: rgb(61, 61, 61);
-  border: 3px solid black;
-  width: 255px;
-  padding-bottom: 60px;
-  height: 70px;
-  font-family: 'Rubbik', Arial;
+    display: none;
+    background: rgb(61, 61, 61);
+    width: 255px;
+    border-radius: 3px;
+    padding-bottom: 60px;
+    height: 50px;
+    top: -5px;
+    left: -2px;
+    font-family: 'Rubbik', Arial;
+    z-index: 100;
 }
 .watch-again-controls button:hover{
   scale: 1.03;
@@ -183,14 +199,13 @@ button{
 }
 
 .play{
-  color: black !important;
-  background: white !important;
-  border: solid 1px white !important;
+    position: absolute;
+    top: 30%;
+    left: 5%;
+    color: black !important;
+    background: white !important;
+    border: solid 1px white !important;
 }
-.play button{
-  background: white !important;
-}
-
 .watch-again-controls .play{
   position: absolute;
   top: 30%;
@@ -203,6 +218,19 @@ button{
   top: 30%;
 
 }
+.fav{
+  position: absolute;
+  right: 20%;
+  top: 30%;
+}
+
+.item-categories{
+    position: absolute;
+  bottom: 9%;
+  left: 5%;
+  color: gray;
+  font-size: 12px;
+}
 .watch-again-item{
     padding: .25rem;
     max-width: 12%;
@@ -212,8 +240,17 @@ button{
     scale: 1.2;
     transition: all .5s ease-in-out;
     transform: translateY(-60px);
+    z-index: 500;
+}
+
+.watch-again-item:hover .watch-again-controls{
+    transition: all .5s ease-in-out;
+    display: block;
+    filter: drop-shadow(2px 2px 4px black);
 }
 .history-bar{
+    position: relative;
+    top: -4px;
     width: 250px;
     height: 1vw;
     max-height: 7px;
@@ -271,7 +308,7 @@ button{
 }
 
 .left-arrow:hover, .right-arrow:hover{
-    opacity: 1;
+    opacity: 1 !important;
     transition: all .3s ease-in-out;
     cursor: pointer;
     background: rgba(0,0,0,.6);
@@ -279,8 +316,9 @@ button{
 
 
 }
-.watch-again-container:hover .side-buttons .left-arrow, .watch-again-container:hover .side-buttons .right-arrow{
-    opacity: .4;
+.watch-again-container:hover .side-buttons .left-arrow, 
+.watch-again-container:hover .side-buttons .right-arrow{
+    opacity: .6;
     transition: all .3s ease-in-out;
 }
 .left-arrow{
