d92e120c417572cee5da78c0fe9ae3858935fc82
BrenanMarenger <bmarenge@nmu.edu>
2023-04-18 22:04:17 -0400

    Fixed css

--- client/src/components/galleryComponents/carousel.vue
+++ client/src/components/galleryComponents/carousel.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div>
+<div class="carousel-body">
     <!-- Index Counter -->
     <div class="index-container" v-if="numIndexes > 1">
             <div v-for="index in (numIndexes + 1)" class="index" @click="changeIndex(index)">
@@ -19,47 +19,49 @@
                 xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"  fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"/></svg>
         </div>
         
-
+        
         <div class="slider" :id="sectionTitle">
-            <div v-for="video in carouselVideos" class="item">
-                <img :src="video.Thumbnail" @click="sendToggleModal(video)"/>
-                <!-- History bar -->
-                <div class="history-bar" v-if="sectionTitle == 'watchagain'">
-                    <div class="watched-bar" :style="{'width': video.Spot}"></div>
-                </div>
-
-                <!-- Hover Menu -->
-                <div class="carousel-controls">
-                    <!-- Title -->
-                    <div class="item-title">
-                        {{ video.Title }}
+            
+                <div v-for="(video, index) in carouselVideos" class="item">
+                    <img :src="video.Thumbnail" @click="sendToggleModal(video)"/>
+                    
+                    <!-- History bar -->
+                    <div class="history-bar" v-if="sectionTitle == 'watchagain'">
+                        <div class="watched-bar" :style="{'width': video.Spot}"></div>
                     </div>
-                    <!-- Play btn -->
-                    <router-link class="play" :to="{name: 'display',  params: {videoId: video.id}, query: { time: video.Spot }}" tag="button">
-                        <svg viewBox="0 0 24 24" >
-                            <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
-                        </svg>
-                    </router-link>
-                    <!-- Modal Btn -->
-                    <button class="more-info" @click="sendToggleModal(video)">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
-                    </button>
-                    <!-- Favorite btn -->
-                    <button class="fav" @click="sendToggleFavorite(video)" v-if="favoritesId.includes(video.id)">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
-                    </button>
-                    <button class="fav" @click="sendToggleFavorite(video)" v-if="!(favoritesId.includes(video.id))">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
-                    </button>
-                    <!-- Categories -->
-                    <div class="item-categories">
-                        {{ video.Categories }}
+
+                    <!-- Hover Menu -->
+                    <div class="carousel-controls">
+                        <!-- Title -->
+                        <div class="item-title">
+                            {{ video.Title }}
+                        </div>
+                        <!-- Play btn -->
+                        <router-link class="play" :to="{name: 'display',  params: {videoId: video.id}, query: { time: video.Spot }}" tag="button">
+                            <svg viewBox="0 0 24 24" >
+                                <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                            </svg>
+                        </router-link>
+                        <!-- Modal Btn -->
+                        <button class="more-info" @click="sendToggleModal(video)">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
+                        </button>
+                        <!-- Favorite btn -->
+                        <button class="fav" @click="sendToggleFavorite(video)" v-if="favoritesId.includes(video.id)">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
+                        </button>
+                        <button class="fav" @click="sendToggleFavorite(video)" v-if="!(favoritesId.includes(video.id))">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                        </button>
+                        <!-- Categories -->
+                        <div class="item-categories">
+                            {{ video.Categories }}
+                        </div>
                     </div>
                 </div>
-            </div>
-
             
         </div>
+       
         
         <!-- Right Button -->
         <div class="side-buttons" v-show="numIndexes > 1">
@@ -163,6 +165,13 @@ export default{
 
 <style scoped>
 
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+
 img{
     width: 250px;
     display: flex;
@@ -189,7 +198,7 @@ img{
 .carousel-container{
     display: flex;
     justify-content: center;
-    padding-bottom: 35px;
+    padding-bottom: 60px;
 }
 
 .slider{
