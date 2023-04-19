99e8bcb3ce0710037eb3c39f4ca6e826c127211e
BrenanMarenger <bmarenge@nmu.edu>
2023-04-18 20:37:53 -0400

    Updated DB name

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,13 +3,13 @@ const path = require('path')
 module.exports = {
     port: process.env.PORT || 8081,
     db: {
-        database: process.env.DB_NAME || 'tabtracker',
-        user: process.env.DB_USER || 'tabtracker',
-        password: process.env.DB_PASS || 'tabtracker',
+        database: process.env.DB_NAME || 'retroflixDB',
+        user: process.env.DB_USER || 'retroflixDB',
+        password: process.env.DB_PASS || 'retroflixDB',
         options: {
             dialect: process.env.DIALECT || 'sqlite',
             host: process.env.HOST || 'localhost',
-            storage: path.resolve(__dirname, '../../tabtracker.sqlite')
+            storage: path.resolve(__dirname, '../../retroflixDB.sqlite')
         }
     },
     authenication: {
